1ab26a67ec994ee0fba7c26001de1c9c281148dd
liuleen <ruchenliu447@gmail.com>
2018-11-22 13:01:42 -0800

    test

--- hangman/App.js
+++ hangman/App.js
@@ -1,29 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {
+  Platform, 
+  StyleSheet, 
+  Text, 
+  View,
+  TouchableOpacity
+} from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>HANGME</Text>
+        <Text style={styles.welcome}>Press START to begin</Text>
+        <TouchableOpacity style={styles.instructions}>START</TouchableOpacity>
       </View>
     );
   }
