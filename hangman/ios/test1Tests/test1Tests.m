2e091a9ba0c5bcadd3556099bebbebeb9373277d
liuleen <ruchenliu447@gmail.com>
2018-12-01 23:24:51 -0800

    new ios dir

--- hangman/ios/test1Tests/test1Tests.m
+++ hangman/ios/test1Tests/test1Tests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface hangmanTests : XCTestCase
+@interface test1Tests : XCTestCase
 
 @end
 
-@implementation hangmanTests
+@implementation test1Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
